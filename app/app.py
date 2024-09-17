2f64ccca318666ebc4dcc0576a41c76dcd592f43
[Heitor Magalhaes] <[heitor.magalc@gmail.com]>
2024-09-17 17:56:16 -0300

    Second feature - upload csv file

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
-from services.services import dashboard_goals 
-
+from services.services import dashboard_goals, upload_csv_file 
+import pandas as pd
+import streamlit as st
 
 
 def show_dashboard() -> None:
     dashboard_goals()
+    st.divider()
+    upload_csv_file()
+    st.divider()
 
 
 if __name__ == "__main__":
--- app/services/services.py
+++ app/services/services.py
@@ -1,9 +1,32 @@
 import streamlit as st
+import pandas as pd
 
 def dashboard_goals() -> None:
+    """
+    Questão num.1: Explicação do Objetivo e Motivação:
+    """
     st.title("Bem-vindos ao,  :green-background[Rio de Janeiro!] ✈️")
 
-    st.markdown("→ O objetivo do dashboard é mostrar a chegada mensal de turistas pelo Rio de Janeiro, por via Aérea, segundo continentes e países de residência permanente, entre 2006-2019 ")
+    st.markdown("→ O objetivo do dashboard é mostrar a chegada mensal de turistas pelo Rio de Janeiro, por via Aérea, segundo continentes e países de residência permanente, [2019.](https://www.data.rio/documents/a6c6c3ff7d1947a99648494e0745046d/about)")
 
     st.markdown("↪ A motivação por trás da escolha dos dados é ver/mostrar os dados de turismo, de estrangeiros, no Rio de Janeiro. Com diferentes funcionalidades que facilitem a compreensão dos dados.")
     ##O objetivo do dashboard é mostrar a chegada mensal de turistas pelo Rio de Janeiro, por via Aérea, segundo continentes e países de residência permanente, entre 2006-2019
+
+
+def upload_csv_file() -> None:
+    """
+    Questão num.2: Realizar Upload de Arquivo CSV:
+    """
+    csv_file = st.file_uploader("Carregue um arquivo .csv", type=["csv"])
+    button = st.button("Click")
+    try:
+        if button:
+            details = {
+                "File Name": csv_file.name,
+                "File Size": csv_file.size
+            }
+            st.write(details)
+            df = pd.read_csv(csv_file)
+            st.dataframe(df)
+    except AttributeError as e: ## Feito com ajuda da IA.
+        st.write(f"Error: {str(e)}")
